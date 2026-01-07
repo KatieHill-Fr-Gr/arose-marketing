b26187f13c91d849b1d8d466b43a67fc94d28a0e
Katie Hill <khill.fr.gr@gmail.com>
2026-01-07 14:46:56 +0000

    Basic setup completed

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './styles/layout.css'
+import './styles/index.css'
+
+import { Routes, Route } from 'react-router-dom'
+
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
+import HomePage from './components/HomePage/HomePage'
+import ProjectPage from './components/ProjectPage/ProjectPage'
+import NotFound from './components/404NotFound/404NotFound'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+        <div className='page-container'>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='/projects/:projectId' element={<ProjectPage />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
