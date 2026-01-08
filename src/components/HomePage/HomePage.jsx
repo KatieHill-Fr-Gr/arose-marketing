94993fc6ccb9f713329ce5838219178b00b5eef4
Katie Hill <khill.fr.gr@gmail.com>
2026-01-08 13:50:24 +0000

    Mobile responsive layout added in Tailwind

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,15 +6,20 @@ const HomePage = () => {
 
     return (
         <main>
-            <section>
-            <div className="h-screen flex items-center justify-start">
+            <section className="h-screen flex items-center justify-start">
+            <div className="w-full max-w-[1280px] mx-auto px-[clamp(1rem,3vw,3rem)]">
             <h1 className="max-w-4xl">Lifecycle Marketing for <span className="italic">high-growth</span> businesses, startups & tech</h1>
             </div>
             </section>
-            <section>
-            <div className="h-screen flex items-center justify-start bg-blue">
-            <h2 className="max-w-4xl w-1/2">Hi, I'm <span className="italic">Emily</span>, a CRM specialist with 10 years of experience</h2>
-            <img src={HandWithLightbulb} alt="Illustration of black and white hand passing a pink lightbulb to another on a light blue background" className="w-1/2" />
+            <section className="h-screen flex items-center justify-start bg-brand-secondary">
+            <div className="w-full max-w-[1280px] mx-auto px-[clamp(1rem,3vw,3rem)] flex flex-col md:flex-row gap-8"> 
+            <h2 className="max-w-4xl md:w-1/2">Hi, I'm <span className="italic">Emily</span>, a CRM specialist with 10 years of experience</h2>
+            </div>
+            </section>
+            <section className="h-screen flex items-center justify-start bg-brand-primary">
+            <div className="w-full max-w-[1280px] mx-auto px-[clamp(1rem,3vw,3rem)] flex flex-col md:flex-row gap-8">
+            <h2 className="max-w-4xl md:w-1/2">Hi, I'm <span className="italic">Emily</span>, a CRM specialist with 10 years of experience</h2>
+            <img src={HandWithLightbulb} alt="Illustration of black and white hand passing a pink lightbulb to another on a light blue background" className="md:w-1/2" />
             </div>
             </section>
         </main >
