95bab4c6c15b9e99cbc18e85a15a85c8917c91ed
Katie Hill <khill.fr.gr@gmail.com>
2026-01-11 15:28:43 +0000

    Animated circles added to background

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,9 +6,12 @@ const HomePage = () => {
 
     return (
         <main>
-            <section className="h-screen flex items-center justify-start">
-            <div className="w-full max-w-[1280px] mx-auto px-[clamp(1rem,3vw,3rem)]">
-            <h1 className="md:max-w-4xl">Lifecycle Marketing for <span className="italic">high-growth</span> businesses, startups & tech</h1>
+            <section className="relative h-screen flex items-center justify-start overflow-hidden">
+            <div className="absolute top-1/2 -translate-y-1/2 right-10 w-125 h-125 bg-brand-secondary rounded-full opacity-0 animate-[slideInRight_0.8s_ease-out_0.2s_forwards]"></div>
+            <div className="absolute top-1/2 -translate-y-1/2 right-80 w-125 h-125 bg-brand-secondary rounded-full opacity-0 animate-[slideInRight_0.8s_ease-out_0.6s_forwards]"></div>
+            <div className="absolute top-1/2 -translate-y-1/2 right-150 w-125 h-125 bg-brand-secondary rounded-full opacity-0 animate-[slideInRight_0.8s_ease-out_1.0s_forwards]"></div>
+            <div className="relative z-10 w-full max-w-[1280px] mx-auto px-[clamp(1rem,3vw,3rem)]">
+            <h1 className="md:max-w-4xl">Lifecycle Marketing and CRM for <span className="italic">high-growth</span> businesses and <span className="italic">start-ups</span></h1>
             </div>
             </section>
             <section className="h-screen flex items-center justify-start bg-brand-secondary">
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
                     <Link to="/" className="text-xl font-bold text-gray-800 hover:text-brand-links transition-colors">Arose Marketing</Link>
                 </div>
 
-                <div className="hidden md:flex gap-6 px-6 py-2 border-2 border-brand-secondary rounded-full">
+                <div className="hidden md:flex gap-6 px-6 py-2 border-2 border-brand-border rounded-full">
                     <Link to="/" className="text-l font-bold text-gray-800 hover:text-brand-links transition-colors">Services</Link>
                     <Link to="/" className="text-l font-bold text-gray-800 hover:text-brand-links transition-colors">Contact</Link>
                 </div>
