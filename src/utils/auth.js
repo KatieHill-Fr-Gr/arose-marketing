0cb81e8c698f88d2dec1fb91c23e35301325fccd
Katie Hill <khill.fr.gr@gmail.com>
2026-01-12 14:48:35 +0000

    Token util file added

---
+++ src/utils/auth.js
@@ -0,0 +1,26 @@
+const tokenName = 'arose-token'
+
+export const setToken = (token) => {
+    localStorage.setItem(tokenName, token)
+}
+
+export const getToken = () => {
+    return localStorage.removeItem(tokenName)
+}
+
+export const removeToken = () => {
+    localStorage.removeItem(tokenName)
+}
+
+export const getUser = () => {
+    const token = getToken()
+    if (!token) return null
+    const payloadString = token.split('.')[1]
+    const { user, exp } = JSON.parse(atob(payloadString))
+    const today = Date.now() / 1000
+    if (today > exp) {
+        removeToken()
+        return null
+    }
+    return user
+}
