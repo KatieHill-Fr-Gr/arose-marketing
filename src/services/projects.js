4f3c19c8430bf5ebbd31d4cc644b1d7cd5818e97
Katie Hill <khill.fr.gr@gmail.com>
2026-01-12 15:10:40 +0000

    Projects Axios calls added to services file

--- src/services/projects.js
+++ src/services/projects.js
@@ -3,3 +3,35 @@ import { getToken } from '../utils/auth'
 
 const BASE_URL = import.meta.env.VITE_API_URL + '/projects'
 
+export const getProjects = () => {
+    return axios.get(BASE_URL)
+}
+
+export const showProject = (projectId) => {
+    return axios.get(`${BASE_URL}/${projectId}`)
+}
+
+export const createProject = (projectId) => {
+    return axios.post(BASE_URL, FormData, {
+        headers: {
+            Authorization: `Bearer`
+        }
+    })
+}
+
+export const updateProject = (projectId, formData) => {
+    const token = getToken()
+    return axios.put(`${BASE_URL}/${projectId}`, formData, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+        })
+    }
+
+export const deleteProject = (projectId) => {
+    return axios.delete(`${BASE_URL}/${projectId}`, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
